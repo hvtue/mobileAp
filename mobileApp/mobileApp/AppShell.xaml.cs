2951b7e6ac4dd77f8876106d89f41b2982954fc0
Admin <30697874+hvtue@users.noreply.github.com>
2023-06-18 22:30:13 +0700

    khoi tao app mobile

---
+++ mobileApp/mobileApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using mobileApp.ViewModels;
+using mobileApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace mobileApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
